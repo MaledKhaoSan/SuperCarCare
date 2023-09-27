b68d394c2be7685a641a0a746cec30af8d989ab7
Nutnicha Feuangfu <aina-123@hotmail.co.th>
2023-09-27 23:36:53 +0700

    yygyujgyu

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const port = process.env.PORT || 3001;
-
+ 
 app.use(express.json()); // Add this line to parse JSON data in the request body
 
 // Add this middleware to enable CORS
